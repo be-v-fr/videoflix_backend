b57a1aef229ff22193e3441a01922295d14e0de2
Bengt Früchtenicht <befru000@gmail.com>
2024-12-01 16:12:14 +0100

    feat: added video signals

--- videos_app/apps.py
+++ videos_app/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class VideosAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'videos_app'
+
+    def ready(self):
+        from . import signals
+        return super().ready()
---
+++ videos_app/signals.py
@@ -0,0 +1,19 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save, post_delete
+from .models import Video
+import os
+from pathlib import Path
+
+@receiver(post_save, sender=Video) 
+def create_video(sender, instance, created, **kwargs):
+    if created:
+        pass # add rendering logic later
+
+@receiver(post_delete, sender=Video) 
+def delete_video(sender, instance, *args, **kwargs):
+    if instance.file:
+        if os.path.isfile(instance.file.path):
+            os.remove(instance.file.path)
+    if instance.thumbnail:
+        if os.path.isfile(instance.thumbnail.path):
+            os.remove(instance.thumbnail.path)
