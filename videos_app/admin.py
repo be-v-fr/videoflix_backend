aa9654425e9399c0d4bad03b84ba249702013bf6
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-01-07 08:32:47 +0100

    feat: added textarea to video creation interface

--- videos_app/admin.py
+++ videos_app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django import forms
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 from .models import Video, VideoCompletion
@@ -9,9 +10,17 @@ class VideoResource(resources.ModelResource):
         model = Video
         exclude = ('duration_in_seconds',)
 
+class VideoAdminForm(forms.ModelForm):
+    description = forms.CharField(widget=forms.Textarea(attrs={'rows': 4, 'cols': 40}))
+    
+    class Meta:
+        model = Video
+        fields = '__all__'
+
 @admin.register(Video)
 class VideoAdmin(ImportExportModelAdmin):
-     resource_class = VideoResource
-     readonly_fields = ('duration_in_seconds',)
+    form = VideoAdminForm
+    resource_class = VideoResource
+    readonly_fields = ('duration_in_seconds',)
 
 admin.site.register(VideoCompletion)
