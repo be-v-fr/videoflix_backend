66b4e27205935192044d82431cccb3e317f98b1e
Bengt Früchtenicht <bengt.fruechtenicht@gmx.de>
2025-01-22 19:35:45 +0100

    refactor: updated frontend base url

--- videoflix/wsgi.py
+++ videoflix/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'videoflix.settings')
-os.environ.setdefault('FRONTEND_BASE_URL', 'http://localhost:4200/')
+os.environ.setdefault('FRONTEND_BASE_URL', 'https://videoflix.bengt-fruechtenicht.de/')
 
 application = get_wsgi_application()
